04969cc788650e3e1b4b1f1d0d43fb1299b5ff1f
Konstantin6487 <konstantin6487@rambler.ru>
2017-05-18 19:10:50 +0400

    rm initial utils

--- src/initial.js
+++
@@ -1,3 +0,0 @@
-export const stepsCount = 3;
-export const minInt = 0;
-export const maxInt = 100;
--- src/utils.js
+++
@@ -1,6 +0,0 @@
-import { minInt, maxInt } from './initial';
-
-export default (min = minInt, max = maxInt) => {
-  const rand = (min - 0.5) + (Math.random() * ((max - min) + 1));
-  return Math.round(rand);
-};
