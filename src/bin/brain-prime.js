76d2a96dbaa29f524263e9cd778b6bc7a08ac2c1
Konstantin6487 <konstantin6487@rambler.ru>
2017-05-20 19:39:52 +0400

    add brain-prime game

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import launchBrainPrime from '../games/prime';
+
+launchBrainPrime();
---
+++ src/games/prime.js
@@ -0,0 +1,17 @@
+import engine from '../engine';
+import { cons, getRandInt } from '../utils';
+
+const brainPrime = () => {
+  const currQuestion = getRandInt(2);
+  const isPrime = (num, i = 2) => {
+    if (i >= num) {
+      return true;
+    }
+    return num % i === 0 ? false : isPrime(num, i + 1);
+  };
+  const correctAnswer = isPrime(currQuestion) ? 'yes' : 'no';
+  const textTask = 'Answer "yes" if the number prime otherwise please answer "no".';
+  return cons(textTask, cons(currQuestion, correctAnswer));
+};
+
+export default () => engine(brainPrime);
