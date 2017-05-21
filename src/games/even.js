0658c493b77f89aafaae5717e8415ffa64e2af51
Konstantin6487 <konstantin6487@rambler.ru>
2017-05-21 18:35:12 +0400

    fixes brain-even

--- src/games/even.js
+++ src/games/even.js
@@ -2,14 +2,9 @@ import engine from '../';
 import { cons, getRandInt } from '../utils';
 
 const brainEven = () => {
-  const getCorrAnswer = (val) => {
-    if (val === 0 || val % 2 === 0) {
-      return 'yes';
-    }
-    return 'no';
-  };
+  const isEven = val => val === 0 || val % 2 === 0;
   const currQuestion = getRandInt();
-  const correctAnswer = getCorrAnswer(currQuestion);
+  const correctAnswer = isEven(currQuestion) ? 'yes' : 'no';
   const textTask = 'Answer "yes" if number even otherwise answer "no".';
   return cons(textTask, cons(currQuestion, correctAnswer));
 };
